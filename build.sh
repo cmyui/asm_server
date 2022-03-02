5e82a1036d7f54a1dc8f8854227ec1c99afcef29
cmyui <cmyuiosu@gmail.com>
2022-03-02 01:50:13 -0500

    optimize for clarity, not size & speed

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-nasm -f elf64 main.asm -o main.o
+# TODO: option to optimize for size & speed (-O3, strip symbols)
+nasm -f elf64 -O0 main.asm -o main.o
 ld main.o -o main.out
