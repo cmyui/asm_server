c543dc3f7c16c7a111ee41c5e2b27e7c8e2f6824
cmyui <cmyuiosu@gmail.com>
2023-05-29 21:48:20 -0400

    build with additional debug info (don't strip symbols)

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 # TODO: option to optimize for size & speed (-O3, strip symbols)
-nasm -f elf64 -O0 main.asm -o main.o
+nasm -f elf64 -F dwarf -O0 -g main.asm -o main.o
 ld main.o -o main.out
